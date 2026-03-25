41a3cc13d4b84389c8775c53b94d481db0dc1f80
mariasteles <telesmariaeduarda12@gmail.com>
2026-03-25 17:13:29 -0300

    Projeto

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const usuariosRoutes = require("./routes/usuarios");
+const usuariosRoutes = require("./routes/usuarios.js");
 
 app.use(express.json());
 app.use(express.static('public'));
